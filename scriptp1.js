1e9365aecee35b35ae31ab537415aa62e4922928
ppalomaa <pandyjem@gmail.com>
2022-07-28 11:43:05 +0100

    Ecofit Website
    V6

--- scriptp1.js
+++ scriptp1.js
@@ -27,3 +27,16 @@ $(function () {
     $('[data-toggle="popover"]').popover({
     })
   })
+
+  // Popover for subscribe button in footer
+function formPopover() {
+	var input = document.getElementById('userinput');
+	console.log(input)
+	var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+	if (input.value.match(validRegex)) {
+		$('#form-popover').popover('toggle');
+	} else {
+	  alert("Invalid email address, please try again.");
+	  $('#form-popover').popover('hide');
+	}
+} 
